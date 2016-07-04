a2835a72493aa96e234c7ccca377ca63949ee42f
ricsi <ricsi@192.168.200.202>
2016-07-04 09:16:20 +0200

    v1.1

--- src/udpstream/com/revkusz/udpserver/Server.java
+++ src/udpstream/com/revkusz/udpserver/Server.java
@@ -4,47 +4,69 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
 import java.awt.image.Raster;
-import java.awt.image.RasterFormatException;
-import java.awt.image.VolatileImage;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
-
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Server {
 	//final változok beállitása
-	public static final int WIDTH = 320;
-	public static final int HEIGHT = 200;
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 480;
 	
 static boolean fut = true;
 static byte[] img;
-static BufferedImage bfimg;
+static int[] kep = new int[640*480];
+static BufferedImage bfimg,cbcrimg;
 static boolean setRequest = false;
 static int count = 0;
 public static double start,diff,wait;
+static byte[] asd = new byte[WIDTH*HEIGHT*4];
+static byte[] ycrbr = new byte[WIDTH*HEIGHT*3];
+static byte[] rgbimg = new byte[WIDTH*HEIGHT*3];
+static int red;
+static int green;
+static int blue;
 
 
 	public static void setImage(byte[] image){
-			img = image;
+		
+		byte[] asd2 = new byte[WIDTH*HEIGHT*3];
+		for (int i = 0; i < (WIDTH*HEIGHT*3); i+=4 ) {
+			//System.out.println(image[0]+" "+image[1]+" "+image[2]+" "+image[3]+" "+image[4]+" "+image[5]+" "+image[6]+" "+image[7]);
+			ycrbr[i]=image[((i/3)*4)];
+			ycrbr[i+1]=image[((i/3)*4)+1];
+			ycrbr[i+2]=image[((i/3)*4)+2];
+	    	int y = ycrbr[i]& 0xFF;
+	    	int cb = ycrbr[i+1] & 0xFF;
+	    	int cr = ycrbr[i+2]& 0xFF;
+	    	//System.out.println(ycrbr[i]+" "+y+" ");
+	    	//System.out.println();
+	    	//System.out.format("\n %x   %x", ycrbr[i],y);
+	    	rgbimg[i] = (byte)( y + 1.402 * (cr-128));
+	    	rgbimg[i+1] =(byte) ( y - 0.34414 *(cb-128)-0.71414 * (cr-128));
+	    	rgbimg[i+2] =(byte) ( y + 1.772 * (cb-128));
+			//kep[i/3] = (((int)y & 0xFF) <<16) | (((int)y & 0xFF)<<8) | (int)y & 0xFF;
+	    	//System.out.println(red+" "+green+" "+blue);
+	    	//kep[i/3] = (((int)cb & 0xFF) <<16) | (((int)cb & 0xFF)<<8) | (int)cb & 0xFF;
+	    	//System.out.println(y+" "+cb+" "+cr+"     ycbcr");
+		}
+		for (int k = 0; k<(WIDTH*HEIGHT*3); k+=3) {
+			kep[k/3] = (((int)rgbimg[k] & 0xFF) <<16) | (((int)rgbimg[k+1] & 0xFF)<<8) | (int)rgbimg[k+2] & 0xFF;
+		}
 
 	}
+	
 	
 	public static void main(String[] args)
 	{
 		img = new byte[WIDTH*HEIGHT*3];
 	    final String title = "Test Window";
-	    //final int width = 320;
-	    //final int height = 200;
 	    
 	    boolean running = true;
 	    
@@ -72,14 +94,45 @@ public static double start,diff,wait;
 	    
 	    canvas.createBufferStrategy(3);
 	    bfimg = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
+	    cbcrimg = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+	    BufferedImage image;
+	    
+	    
+	    /*byte[] asd2 = new byte[WIDTH*HEIGHT*3];
+	    for (int i = 0; i < (WIDTH*HEIGHT*3); i+=3 ) {	    	
+	    	asd2[i]=asd[((i/3)*4)+0];
+	    	asd2[i+1]=asd[((i/3)*4)+1];
+	    	asd2[i+2]=asd[((i/3)*4)+2];
+	    	double y = asd2[i] & 0xFF;
+	    	//double cr = asd2[i+1]& 0xFF;
+	    	//double cb = asd2[i+2]& 0xFF;
+	    	//System.out.println(y+" "+cr+" "+cb);
+	    	byte red =(byte)  ((float)cr*((float)2-(float)2*(float)0.299)+(float)y);
+	    	byte blue = (byte) ((float)cb*((float)2-(float)2*(float)0.114)+(float)y);
+	    	byte green = (byte) (((float)y-(float)0.114*(float)blue-(float)0.229*(float)red)/(float)0.587);
+	    	//red=(byte)y;
+	    	//blue=green=50;
+	    	//System.out.println(red+" "+green+" "+blue);
+	    	//System.out.format("%x %x %x \n\r", red,green,blue);
+	    	//kep[i/3] = (red & 0xFF <<16) | (green & 0xFF<<8) | blue & 0xFF;
+	    	kep[i/3] = (((int)y & 0xFF) <<16) | (((int)y & 0xFF)<<8) | (int)y & 0xFF;
+
+	    }*/
+	
+	    //cbcrimg.setData(Raster.createRaster(cbcrimg.getSampleModel(), new DataBufferByte(asd2, asd2.length), new Point() ) );
+	    //cbcrimg.setData(Raster.createRaster(cbcrimg.getSampleModel(), new DataBufferInt(kep, kep.length), new Point() ) );
+	    
 	    
 	    while (running) {	
 	    	
 	        bufferStrategy = canvas.getBufferStrategy();
 	        graphics = bufferStrategy.getDrawGraphics();
-			bfimg.setData(Raster.createRaster(bfimg.getSampleModel(), new DataBufferByte(img, img.length), new Point() ) );
-		    graphics.drawImage(bfimg, 0, 0, null);
-
+	        //System.out.println(image.getHeight());
+			//bfimg.setData(Raster.createRaster(bfimg.getSampleModel(), new DataBufferByte(img, img.length), new Point() ) );
+		    //graphics.drawImage(bfimg, 0, 0, null);
+	        
+	        cbcrimg.setData(Raster.createRaster(cbcrimg.getSampleModel(), new DataBufferInt(kep, kep.length), new Point() ) );
+			graphics.drawImage(cbcrimg, 0, 0, null);
 
 	        
 	        /*
--- src/udpstream/com/revkusz/udpserver/client.java
+++ src/udpstream/com/revkusz/udpserver/client.java
@@ -5,6 +5,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 public class client implements Runnable {
 	
@@ -14,66 +16,70 @@ public class client implements Runnable {
 	byte[] img;
 	boolean run = true;
 	boolean done;
+	
+	
 	@Override
-	public void run() {
+	public void run() {	
+		byte[] img = new byte[(Server.WIDTH*Server.HEIGHT*4)];
 		System.out.println("Receive started...");
-		byte[] receiveData = new byte[Server.WIDTH*3];
+		byte[] receiveData = new byte[1514];
 		receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		/*try {
-			clientSocket.receive(receivePacket);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
+		try {
+			IPAddress = InetAddress.getByName("192.168.200.33");
+		} catch (UnknownHostException e1) {
 			e1.printStackTrace();
-		}*/
-		while (run){
-			try {
-				done = false;
-				//System.out.println("received packet");
-				//if ((receiveData[0] == 7) && (receiveData[1] == 13)) {
-					int i = 0;
-					while (( i<Server.HEIGHT) && ((receiveData[0] != 7) && (receiveData[1] != 13))){
-						clientSocket.receive(receivePacket);
-						//System.out.println("received packet");
-						for (int j=0; j<Server.WIDTH*3; j++){
-					    	img[j+i*(Server.WIDTH*3)] =(byte) (receiveData[j] & 0xFF);
-					    	//System.out.println(receiveData[j]);  
-						}
-						i++;
-					}
-					done = true;
-				/*}*/
-				if (done) {
-					//System.out.println("Frame Done.");
-					Server.setImage(img);
-				}
-				
-			} catch (IOException e) {
+		}
+		String data = "get w 640 h 480 x 0 y 0";
+		byte[] sendData = data.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 4000);
+		try { 
+		    
+		    while(true) {  	
+				clientSocket.send(sendPacket);
+		    	run=true;
+		    	int i = 0;
+		    	while(run){
+		    		try {
+			            clientSocket.receive(receivePacket);
+			            /*if (i ==	0)
+		    				clientSocket.setSoTimeout(1);*/
+			        } catch (SocketTimeoutException e) {
+			           run = false;
+			        }
+		    		if (run) {
+		    			int lenght = receivePacket.getLength();
+		    			for (int j = 0; j<lenght; j++) {
+			    			img[i]=receiveData[j];		   
+			    			i++;
+			    			if (i%1000 == 0 ) {
+			    				//System.out.println(i);
+			    			}
+		    			}
+		    		}
+		    			    		
+		    	}  
+		    	//System.out.println("time out");
+				Server.setImage(img);
+		    }
+		} catch (IOException e) {
 			
-			}
 		}
 		clientSocket.close();
 	}
 	
 	client() {
+		
+		//img = new byte [Server.WIDTH*Server.HEIGHT*3];
 		try {
-		    byte[] sendData = new byte[1];
-		    img = new byte [Server.WIDTH*Server.HEIGHT*3];
-			sendData[0] = 120;
-			
-			try {
-				clientSocket = new DatagramSocket(60501);
-			} catch (SocketException ex) {
-				ex.printStackTrace();
-			}
-			//IPAddress = InetAddress.getByName("192.168.200.239");
-			IPAddress = InetAddress.getByName("192.168.1.10");
-		    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		    clientSocket.send(sendPacket);
-		    System.out.println("Begin package sent");
-		    
-		} catch (IOException e) {
-			
+			clientSocket = new DatagramSocket(4002);
+			clientSocket.setSoTimeout(30);
+		} catch (SocketException ex) {
+			ex.printStackTrace();
 		}
+
+		//IPAddress = InetAddress.getByName("192.168.200.239");
+		
+		System.out.println("Begin package sent");
 	    
 	}
 
